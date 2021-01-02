298b9d1119dacf34fe5cbfa136b7df8e8bb08939
TubaGunes <tuba21@gmail.com>
2021-01-02 16:10:47 -0500

    Init

---
+++ src/CodingBat_Practices/WarmUp1.java
@@ -0,0 +1,11 @@
+package CodingBat_Practices;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class WarmUp1 {
+
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        return ( !weekday || vacation);
+
+    }
+}
